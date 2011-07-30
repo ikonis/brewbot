7c44783b8f650249c52288cb81764de7469f6886
ikonis <ikonis@gmail.com>
2011-07-30 14:28:31 -0500

    Dunno if it works or not.  But hey.

--- weather.rb
+++ weather.rb
@@ -13,18 +13,37 @@ class Weather
   def execute(m,location)
     location=location.gsub(' ','+')
     xml=Net::HTTP.get(URI.parse("http://www.google.com/ig/api?weather=#{location}"))
-
     doc = Hpricot::XML(xml)
-   (doc/:current_conditions).each do |status|
-     first=true
-     (status/"*").each do |el|
-       if first
-         first=false
-       else
-         m.reply el.to_s
-             #m.reply "#{el}: #{status.at(el).innerHTML}"
-       end
-     end
+    info=doc.search("/xml_api_reply/weather/forecast_information")
+    current=doc.search("/xml_api_reply/weather/current_conditions")
+    cityName=info[0].innerHTML
+    currentTemp=current[1].innerHTML
+    currentHumidity=current[3].innerHTML
+    m.reply "#{cityName}   Current Temp: #{currentTemp}   #{currentHumidity}
+
+    =begin
+    (doc/:forcast_information).each do |status|
+      first=true
+      (status/"*".each do |el|
+        if first
+          first=false
+        else
+          m.reply el.to_s
+        end
+      end
     end
-  end
+    (doc/:current_conditions).each do |status|
+      first=true
+      (status/"*").each do |el|
+        if first
+          first=false
+        else
+          m.reply el.to_s
+          #m.reply "#{el}: #{status.at(el).innerHTML}"
+        end
+      end
+     end
+   end
+   =end
+
 end
